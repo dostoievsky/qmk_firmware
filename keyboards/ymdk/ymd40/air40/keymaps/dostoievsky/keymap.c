491fe4634ba09b00551da33e4de4aa9a3fd90670
cortexmancer <git@thecortexmancer.org>
2024-05-14 10:37:12 -0300

    chore: remove tap dances and simplify some key definitions

--- keyboards/ymdk/ymd40/air40/keymaps/dostoievsky/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/dostoievsky/keymap.c
@@ -15,78 +15,10 @@
  */
 
 #include QMK_KEYBOARD_H
-#include "quantum.h"
 
 void keyboard_post_init_user(void) {
     rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
 }
-
-const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
-
-// This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &delete_key_override,
-    NULL // Null terminate the array of overrides!
-};
-
-#define KC_CCED ALGR(KC_COMMA)
-
-enum {
-    CT_CLN,
-};
-
-typedef struct {
-    uint16_t tap;
-    uint16_t hold;
-    uint16_t held;
-} tap_dance_tap_hold_t;
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    tap_dance_action_t *action;
-    switch (keycode) {
-        case TD(CT_CLN):
-            action = &tap_dance_actions[TD_INDEX(keycode)];
-            if (!record->event.pressed && action->state.count && !action->state.finished) {
-                tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
-                tap_code16(tap_hold->tap);
-            }
-    }
-    return true;
-}
-
-void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
-    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
-
-    if (state->pressed) {
-        if (state->count == 1
-#ifndef PERMISSIVE_HOLD
-            && !state->interrupted
-#endif
-        ) {
-            register_code16(tap_hold->hold);
-            tap_hold->held = tap_hold->hold;
-        } else {
-            register_code16(tap_hold->tap);
-            tap_hold->held = tap_hold->tap;
-        }
-    }
-}
-
-void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
-    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
-
-    if (tap_hold->held) {
-        unregister_code16(tap_hold->held);
-        tap_hold->held = 0;
-    }
-}
-
-#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold) \
-    { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}), }
-
-tap_dance_action_t tap_dance_actions[] = {
-    [CT_CLN] = ACTION_TAP_DANCE_TAP_HOLD(KC_COLN, KC_SCLN),
-};
 
 enum layer_names {
   _QWERTY,
@@ -94,19 +26,23 @@ enum layer_names {
   _RAISE,
   _ADJUST
 };
+     
+#define KC_CCED ALGR(KC_COMMA)
+#define KC_LOWER LT(1, KC_DEL)
+#define KC_RAISE LT(2, KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_ortho_4x12(
-        KC_ESC,         KC_Q,    KC_W,    KC_E,    KC_R,    		KC_T,    KC_Y,    KC_U,           	KC_I,    KC_O,     KC_P,       KC_BSLS,
-        KC_TAB,         KC_A,    KC_S,    KC_D,    KC_F,    		KC_G,    KC_H,    KC_J,           	KC_K,    KC_L,     TD(CT_CLN), KC_QUOT,
-        KC_LSFT,	KC_Z,    KC_X,    KC_C,    KC_V,    		KC_B,    KC_N,    KC_M,           	KC_COMM, KC_DOT,   KC_SLSH,    KC_ENT,
-        KC_LCTL,	MO(3),   KC_LGUI, KC_LALT, LT(1, KC_DEL),	KC_SPC,  KC_SPC,  LT(2, KC_BSPC),	KC_LEFT, KC_DOWN,  KC_UP,      KC_RGHT
+        KC_ESC,         KC_Q,    KC_W,    KC_E,    KC_R,    	KC_T,    KC_Y,    KC_U,		KC_I,    KC_O,     KC_P,       	KC_BSLS,
+        KC_TAB,         KC_A,    KC_S,    KC_D,    KC_F,    	KC_G,    KC_H,    KC_J,		KC_K,    KC_L,     KC_SCLN,	KC_QUOT,
+        KC_LSFT,	KC_Z,    KC_X,    KC_C,    KC_V,    	KC_B,    KC_N,    KC_M,		KC_COMM, KC_DOT,   KC_SLSH,    	KC_ENT,
+        KC_LCTL,	MO(3),   KC_LGUI, KC_LALT, KC_LOWER,	KC_SPC,  KC_SPC,  KC_RAISE,	KC_LEFT, KC_DOWN,  KC_UP,      	KC_RGHT
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, S(KC_MINS), KC_EQL,
         KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, KC_LCBR, KC_RCBR,    KC_GRV,
         _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______,    _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,      KC_RGHT
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PMNS, KC_PPLS,
