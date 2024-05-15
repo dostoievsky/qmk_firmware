c409ae3f34a3c1dbc5abb520e3b6c6944d408e14
cortexmancer <git@thecortexmancer.org>
2024-05-15 11:47:19 -0300

    chore: add right controlers in qwerty mode to better control

--- keyboards/ymdk/ymd40/air40/keymaps/dostoievsky/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/dostoievsky/keymap.c
@@ -31,6 +31,7 @@ enum layer_names {
 #define KC_LR LT(1, KC_DEL)
 #define KC_RE LT(2, KC_BSPC)
 #define OSM_LSFT OSM(MOD_LSFT)
+#define OSM_RSFT OSM(MOD_RSFT)
 #define KC_CCED ALGR(KC_COMMA)
 #define UNDO
 
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT_ESC,     KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,     KC_BSLS,
         KC_TAB,     KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
         OSM_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_ENT,
-        KC_LCTL,    OSL(3),  KC_LGUI, KC_LALT, KC_LR, KC_SPC,  KC_SPC,  KC_RE, KC_LEFT, KC_DOWN,  KC_UP,    KC_RGHT
+        KC_LCTL,    OSL(3),  KC_LGUI, KC_LALT, KC_LR, KC_SPC,  KC_SPC,  KC_RE, KC_RALT, KC_MENU,  KC_RCTL,  OSM_RSFT
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, S(KC_MINS), KC_EQL,
