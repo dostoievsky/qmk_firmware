6fc31353b2379b038f45ebdeaf3c4701c6c015c6
cortexmancer <git@thecortexmancer.org>
2024-05-15 09:44:38 -0300

    chore: lint keys

--- keyboards/ymdk/ymd40/air40/keymaps/dostoievsky/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/dostoievsky/keymap.c
@@ -26,17 +26,18 @@ enum layer_names {
   _RAISE,
   _ADJUST
 };
-     
+
 #define KC_CCED ALGR(KC_COMMA)
 #define KC_LR LT(1, KC_DEL)
 #define KC_RE LT(2, KC_BSPC)
+#define OSM_LSFT OSM(MOD_LSFT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_ortho_4x12(
-        KC_ESC,         KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,	KC_BSLS,
-        KC_TAB,         KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,	KC_QUOT,
-        OSM(MOD_LSFT),	KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,	KC_ENT,
-        KC_LCTL,	MO(3),   KC_LGUI, KC_LALT, KC_LR, KC_SPC,  KC_SPC,  KC_RE, KC_LEFT, KC_DOWN,  KC_UP,	KC_RGHT
+        KC_ESC,     KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,     KC_BSLS,
+        KC_TAB,     KC_A,    KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
+        OSM_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_ENT,
+        KC_LCTL,    MO(3),   KC_LGUI, KC_LALT, KC_LR, KC_SPC,  KC_SPC,  KC_RE, KC_LEFT, KC_DOWN,  KC_UP,    KC_RGHT
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
         KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, S(KC_MINS), KC_EQL,
@@ -46,15 +47,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PMNS, KC_PPLS,
-        -------, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, KC_LBRC, KC_RBRC, _______,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, KC_LBRC, KC_RBRC, _______,
         _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, KC_CCED, _______,
         _______, _______, _______, _______, KC_BSPC, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END
     ),
     [_ADJUST] = LAYOUT_ortho_4x12(
-        _______, _______, G(KC_1), G(KC_2), G(KC_3), _______, _______, _______, _______, _______, KC_PSCR, C(KC_SCLN),
-        _______, _______, G(KC_4), G(KC_5), G(KC_6), _______, _______, _______, _______, _______, _______, _______,
-        KC_LSFT, _______, G(KC_7), G(KC_8), G(KC_9), _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, G(KC_0), _______, _______, _______, _______, _______, _______, _______, _______
+        _______, _______, _______, _______, _______, _______, _______, G(KC_1), G(KC_2), G(KC_3), KC_PSCR, C(KC_SCLN),
+        _______, _______, _______, _______, _______, _______, _______, G(KC_4), G(KC_5), G(KC_6), _______, _______,
+        KC_LSFT, _______, _______, _______, _______, _______, _______, G(KC_7), G(KC_8), G(KC_9), _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, G(KC_0), _______, _______, _______
     )
 };
 
