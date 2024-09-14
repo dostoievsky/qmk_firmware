13396fd06d8929e3bfaa7f83f79aa02671015e78
cortexmancer <git@thecortexmancer.org>
2024-09-14 13:55:04 -0300

    chore: control layer use row numbers and func key trigger control layer

--- keyboards/ymdk/ymd40/air40/keymaps/dostoievsky/keymap.c
+++ keyboards/ymdk/ymd40/air40/keymaps/dostoievsky/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LT_ESC,     KC_Q,     KC_W,    KC_E,    KC_R,  KC_T,    KC_Y,    KC_U,  KC_I,    KC_O,     KC_P,     KC_BSLS,
         KC_TAB,     KC_A,     KC_S,    KC_D,    KC_F,  KC_G,    KC_H,    KC_J,  KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
         OSM_LSFT,   KC_Z,     KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_ENT,
-        KC_LCTL,    _______,  KC_LGUI, KC_LALT, KC_LR, KC_SPC,  KC_SPC,  KC_RE, KC_RALT, KC_MENU,  KC_RCTL,  OSM_RSFT
+        KC_LCTL,    OSL(3),  KC_LGUI, KC_LALT, KC_LR, KC_SPC,  KC_SPC,  KC_RE, KC_RALT, KC_MENU,  KC_RCTL,  OSM_RSFT
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
         KC_PLUS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
@@ -54,10 +54,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,   _______, _______, _______, KC_BSPC, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END
     ),
     [_CONTROL] = LAYOUT_ortho_4x12(
-        _______, _______, _______, _______, _______, _______, _______, G(KC_1), G(KC_2), G(KC_3), KC_PSCR, C(KC_SCLN),
-        _______, _______, _______, _______, _______, _______, _______, G(KC_4), G(KC_5), G(KC_6), _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, G(KC_7), G(KC_8), G(KC_9), _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, G(KC_0), _______, _______, _______
+        _______, G(KC_1), G(KC_2), G(KC_3), G(KC_4), G(KC_5), G(KC_6), G(KC_7), G(KC_8), G(KC_9), G(KC_0), _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     )
 };
 
