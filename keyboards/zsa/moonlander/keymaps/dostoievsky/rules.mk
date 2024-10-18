232ff90881f38b1891095aee384fcb528dfac125
cortexmancer <git@thecortexmancer.org>
2024-10-18 09:59:33 -0300

    chore: remove auto shift

--- keyboards/zsa/moonlander/keymaps/dostoievsky/rules.mk
+++ keyboards/zsa/moonlander/keymaps/dostoievsky/rules.mk
@@ -2,7 +2,7 @@
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
-AUTO_SHIFT_ENABLE = yes
+AUTO_SHIFT_ENABLE = no
 MOUSEKEY_ENABLE = no
 ORYX_ENABLE = yes
 RGB_MATRIX_CUSTOM_KB = no
